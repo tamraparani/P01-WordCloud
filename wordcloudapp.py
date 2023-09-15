da1311dabc0d518a21b0a8b81c8629dcee489b43
Deepa Tamraparani <tamraparani.d@gmail.com>
2023-09-15 12:25:38 +0200

    default

--- wordcloudapp.py
+++ wordcloudapp.py
@@ -10,8 +10,7 @@ from nltk.corpus import stopwords
 
 # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
-text = st.text_area('Write your article')
-
+text = st.text_area('Write your article','WELCOME')
 # Read the whole text.
 #text = open(path.join(d, 'article.txt')).read()
 #stopwords = nltk.download('stopwords')
@@ -26,5 +25,6 @@ plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['font.sans-serif'] = 'Arial'
 # Define a TrueType font file (you can change this to your preferred font)
 font_path = path.join(d, 'Roboto-Light.ttf')
+
 wordcloud = WordCloud(font_path=font_path).generate(text)
 st.image(wordcloud.to_array(), use_column_width=True)
