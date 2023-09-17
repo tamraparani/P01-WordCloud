383a78fa41f7a89bd62d379d5550b7c177e3f7da
Deepa Tamraparani <tamraparani.d@gmail.com>
2023-09-17 14:05:26 +0200

    manasa bday

--- wordcloudapp.py
+++ wordcloudapp.py
@@ -10,7 +10,7 @@ from nltk.corpus import stopwords
 
 # get data directory (using getcwd() is needed to support running example in generated IPython notebook)
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
-text = st.text_area('Write your article','WELCOME')
+text = st.text_area('Write your article','HAPPY BIRTHDAY MANASA')
 # Read the whole text.
 #text = open(path.join(d, 'article.txt')).read()
 #stopwords = nltk.download('stopwords')
